9ecd617a726d5f4958986e80e65def9590f325cf
David Mittelstädt <cpt.dave@web.de>
2019-01-06 23:05:55 +0100

    Edit Dockerfile for normalizing file names; Edit dump-files for writing files and dirs to file

--- python/normalize/bin/dump-files.sh
+++ python/normalize/bin/dump-files.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
+#
+# Creates executables to create directory structures
+#
+# Author: David Mittelstaedt  <cpt.dave@web.de>
+# Date: 2019-01-06
 
 BASE_DIR=/home/david/Music
-DIR_OUT=/home/david/Documents/music-dirs.txt
-FILES_OUT=/home/david/Documents/music-files.txt
+
+BASE_DOCKER_DIR=/data
+
+# Current directory
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DIR_OUT=${CURRENT_DIR}/create-dirs.sh
+FILES_OUT=${CURRENT_DIR}/create-files.sh
 
 printf "Dump files and directories...\n"
 
+echo "#!/usr/bin/env bash" > ${DIR_OUT}
+echo "#!/usr/bin/env bash" > ${FILES_OUT}
+
 IFS=$'\n'
-dirs=($(find ${BASE_DIR} -mindepth 2 -maxdepth 2 -type d | shuf | head -n 15))
+dirs=($(find ${BASE_DIR} -mindepth 2 -maxdepth 2 -type d | shuf | head -n 5))
 
 for dir in "${dirs[@]}"; do
   cd ${dir}
   directory=$(pwd | cut -d / -f4-9)
-  printf "${directory}\n" >> ${DIR_OUT}
+  printf "mkdir -p '${BASE_DOCKER_DIR}/${directory}'\n" >> ${DIR_OUT}
   files=($(find -type f -printf "%f\n" | sort))
   for file in "${files[@]}"; do
-    printf "${directory}/${file}\n" >> ${FILES_OUT}
+    printf "touch $\"${BASE_DOCKER_DIR}/${directory}/${file}\"\n" >> ${FILES_OUT}
   done
 done
+
+sed -i 's/(/\\\(/g' ${FILES_OUT}
+sed -i 's/)/\\\)/g' ${FILES_OUT}
+sed -i 's/(/\\\(/g' ${DIR_OUT}
+sed -i 's/)/\\\)/g' ${DIR_OUT}
+
+chmod 750 ${DIR_OUT} ${FILES_OUT}
 
 printf "Finished dumping files and directories!\n"
 
 exit 0
+
+
+# sed -i.bak 's/(/\\\(/g' test.tx

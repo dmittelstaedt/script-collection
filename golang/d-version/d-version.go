f2df0117aebbb012665164859d1df4770148a299
David Mittelstädt <cpt.dave@web.de>
2019-04-04 17:35:09 +0200

    Remove json config file; Add yaml config file; Add scraping for local file; Update struct with bool flag for searching locally; Add Dockerfile

--- golang/d-version/d-version.go
+++ golang/d-version/d-version.go
@@ -3,27 +3,34 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/viper"
+
+	"github.com/gocolly/colly"
 )
 
+const configurationFileName = "config"
+
 type configuration struct {
+	RemoteVersionURL  string
 	CurrentVersionURL string
 	IndexHTMLFile     string
 	SearchString      string
+	UseLocal          bool
 }
 
-func readConfig() configuration {
+func readConfig(configurationFilename string) configuration {
 	var configuration configuration
 
-	data, err := ioutil.ReadFile("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+	viper.SetConfigName(configurationFilename)
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
 
-	err = json.Unmarshal(data, &configuration)
+	err := viper.Unmarshal(&configuration)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,8 +38,37 @@ func readConfig() configuration {
 	return configuration
 }
 
-func getCurrentVersion() {
+func getCurrentVersion(currentVersionURL string) string {
+	var currentVersion string
 
+	collector := colly.NewCollector()
+	collector.OnHTML("p[class=\"small text-center\"]", func(e *colly.HTMLElement) {
+		currentVersion = e.Text
+	})
+
+	collector.Visit(currentVersionURL)
+
+	return strings.TrimSpace(currentVersion)
+}
+
+func getCurrentVersionLocal() string {
+	var currentVersion string
+
+	fileTransport := &http.Transport{}
+	fileTransport.RegisterProtocol("file", http.NewFileTransport(http.Dir("./")))
+
+	collector := colly.NewCollector()
+	collector.WithTransport(fileTransport)
+	collector.OnHTML("p[class=\"small text-center\"]", func(e *colly.HTMLElement) {
+		currentVersion = e.Text
+	})
+
+	err := collector.Visit("file://./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.TrimSpace(currentVersion)
 }
 
 func getRemoteVersion() {
@@ -44,6 +80,8 @@ func updateCurrentVersion() {
 }
 
 func main() {
-	configuration := readConfig()
-	fmt.Printf("currentVersionURL: %v\n", configuration.CurrentVersionURL)
+	configuration := readConfig(configurationFileName)
+	fmt.Printf("Current Version URL: %v\n", configuration.CurrentVersionURL)
+	currentVersion := getCurrentVersion(configuration.CurrentVersionURL)
+	fmt.Printf("Current Version: %v\n", currentVersion)
 }
